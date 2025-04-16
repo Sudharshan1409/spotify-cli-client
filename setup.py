2ca7b98d2c344b4d5bc12198b1ae882d42eedbec
Sudharshan V <sudarshan61kv@gmail.com>
2025-04-16 22:11:21 +0530

    reconfigured cli

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="spotipy-cli-client",
-    version="0.1.2",
+    name="spotify-cli-client",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=["click", "spotipy", "InquirerPy"],
     entry_points={
